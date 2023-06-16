5a333ca61ad9c08033d1331a71194eaa1afbf2f8
arif wijanarko <arifpethod@gmail.com>
2023-06-16 09:25:01 +0700

    tugas javascript 22
    untuk niomic

---
+++ tugas22.js
@@ -0,0 +1,12 @@
+function tugas22() {
+    var kalimat = "Saya ingin belajar bersama";
+
+    var arr = kalimat.split(" ");
+
+    arr.forEach((a, b) => {
+        console.log("item :", a, "Index ke ", b);
+        
+    });
+}
+
+tugas22();
